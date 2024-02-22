ef23be7b55185a69940b700f8502adfa9d92bfcc
Pritam <Pritam@gmail.com>
2024-02-22 16:51:22 +0530

    some changes

--- bootjpaexample/src/main/java/com/jpa/test/BootjpaexampleApplication.java
+++ bootjpaexample/src/main/java/com/jpa/test/BootjpaexampleApplication.java
@@ -2,22 +2,27 @@ package com.jpa.test;
 
 import org.springframework.boot.SpringApplication;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
-import org.springframework.context.ConfigurableApplicationContext;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.context.ApplicationContext;
 import com.jpa.test.dao.UserRepository;
+import com.jpa.test.entities.User;
+
 @SpringBootApplication
 public class BootjpaexampleApplication {
 
 	public static void main(String[] args) {
-	ConfigurableApplicationContext context=	SpringApplication.run(BootjpaexampleApplication.class, args);
+	ApplicationContext context=	SpringApplication.run(BootjpaexampleApplication.class, args);
 	UserRepository userRepository = context.getBean(UserRepository.class);
 	
 	User user = new User();
 	user.setName("Pritam Chanda");
 	user.setCity("Kolkata");
 	user.setStatus("I am Java programmer");
+	
+	User user1=userRepository.save(user);
+	System.out.print(user1);
 	}
 
 }
